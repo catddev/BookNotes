150c5b394175ee7c9ca9f71f10a9b29618dded8b
Dinara Aikenova <aikenova01@gmail.com>
2024-05-26 21:31:40 +0500

    Add theme, styles, colors, etc

--- app/src/main/java/com/epamupskills/booknotes/ui/theme/Theme.kt
+++ app/src/main/java/com/epamupskills/booknotes/ui/theme/Theme.kt
@@ -12,15 +12,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlueGrey900,
+    secondary = BlueGrey700,
+    tertiary = BlueGrey800,
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = BlueGrey900,
+    secondary = BlueGrey700,
+    tertiary = BlueGrey800,
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
