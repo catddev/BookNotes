c6acbd649ad04757a75508ae4bcfa04617debbaf
Dinara Aikenova <aikenova01@gmail.com>
2024-05-27 15:30:53 +0500

    Change dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -43,6 +46,9 @@ dependencies {
     api(libs.material)
     api(libs.androidx.activity)
     api(libs.androidx.constraintlayout)
+
+    //fragment
+    api(libs.androidx.fragment.ktx)
 
     //lifecycle
     api(libs.androidx.lifecycle.runtime.ktx)
@@ -62,7 +68,8 @@ dependencies {
     api(libs.lottie.compose)
 
     // navigation
-    api(libs.navigation.compose)
+    api(libs.navigation.fragment.ktx)
+    api(libs.navigation.ui.ktx)
 
     // serialization
     api(libs.kotlinx.serialization)
@@ -73,10 +80,14 @@ dependencies {
     annotationProcessor(libs.androidx.room.compiler)
     api(libs.androidx.room.ktx)
 
+    //datastore
+    api(libs.datastore.prefs)
+    api(libs.datastore)
+
     // hilt
     ksp(libs.hilt.compiler)
     api(libs.hilt)
-    api(libs.hilt.navigation.compose)
+    api(libs.hilt.nav.fragment)
 
     //firebase
     api(platform(libs.firebase.bom))
